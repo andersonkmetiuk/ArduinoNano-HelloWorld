0e1b13a444889f7274573bb2f0f3e1e1e9497bf2
Anderson Kmetiuk <58405194+andersonkmetiuk@users.noreply.github.com>
2023-09-17 22:45:05 -0300

    pings-example

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,63 @@
-// Present a "Will be back soon web page", as stand-in webserver.
-// 2011-01-30 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
-#include <Arduino.h>
+// Ping a remote server, also uses DHCP and DNS.
+// 2011-06-12 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
+
 #include <EtherCard.h>
+#include <Arduino.h>
 
-#define STATIC 1  // set to 1 to disable DHCP (adjust myip/gwip values below)
-
-#if STATIC
-// ethernet interface ip address
-static byte myip[] = { 192,168,0,200 };
-// gateway ip address
-static byte gwip[] = { 192,168,0,1 };
-#endif
-
-// ethernet mac address - must be unique on your network
+// ethernet interface mac address, must be unique on the LAN
 static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
 
-byte Ethernet::buffer[500]; // tcp/ip send and receive buffer
+byte Ethernet::buffer[700];
+static uint32_t timer;
 
-const char page[] PROGMEM =
-"HTTP/1.0 503 Service Unavailable\r\n"
-"Content-Type: text/html\r\n"
-"Retry-After: 600\r\n"
-"\r\n"
-"<html>"
-  "<head><title>"
-    "Service Temporarily Unavailable"
-  "</title></head>"
-  "<body>"
-    "<h3>This service is currently unavailable</h3>"
-    "<p><em>"
-      "The main server is currently off-line.<br />"
-      "Please try again later."
-    "</em></p>"
-  "</body>"
-"</html>"
-;
-
-void setup(){
-  Serial.begin(57600);
-  Serial.println("\n[backSoon]");
-  
-  if (ether.begin(sizeof Ethernet::buffer, mymac) == 0) 
-    Serial.println( "Failed to access Ethernet controller");
-#if STATIC
-  ether.staticSetup(myip, gwip);
-#else
-  if (!ether.dhcpSetup())
-    Serial.println("DHCP failed");
-#endif
-
-  ether.printIp("IP:  ", ether.myip);
-  ether.printIp("GW:  ", ether.gwip);  
-  ether.printIp("DNS: ", ether.dnsip);  
+// called when a ping comes in (replies to it are automatic)
+static void gotPinged (byte* ptr) {
+  ether.printIp(">>> ping from: ", ptr);
 }
 
-void loop(){
-  // wait for an incoming TCP packet, but ignore its contents
-  if (ether.packetLoop(ether.packetReceive())) {
-    memcpy_P(ether.tcpOffset(), page, sizeof page);
-    ether.httpServerReply(sizeof page - 1);
+void setup () {
+  Serial.begin(57600);
+  Serial.println("\n[pings]");
+  
+  if (ether.begin(sizeof Ethernet::buffer, mymac) == 0)
+    Serial.println(F("Failed to access Ethernet controller"));
+  if (!ether.dhcpSetup())
+    Serial.println(F("DHCP failed"));
+
+  ether.printIp("IP:  ", ether.myip);
+  ether.printIp("GW:  ", ether.gwip);
+
+#if 1
+  // use DNS to locate the IP address we want to ping
+  if (!ether.dnsLookup(PSTR("www.google.com")))
+    Serial.println("DNS failed");
+#else
+  ether.parseIp(ether.hisip, "74.125.77.99");
+#endif
+  ether.printIp("SRV: ", ether.hisip);
+    
+  // call this to report others pinging us
+  ether.registerPingCallback(gotPinged);
+  
+  timer = -9999999; // start timing out right away
+  Serial.println();
+}
+
+void loop () {
+  word len = ether.packetReceive(); // go receive new packets
+  word pos = ether.packetLoop(len); // respond to incoming pings
+  
+  // report whenever a reply to our outgoing ping comes back
+  if (len > 0 && ether.packetLoopIcmpCheckReply(ether.hisip)) {
+    Serial.print("  ");
+    Serial.print((micros() - timer) * 0.001, 3);
+    Serial.println(" ms");
+  }
+  
+  // ping a remote server once every few seconds
+  if (micros() - timer >= 5000000) {
+    ether.printIp("Pinging: ", ether.hisip);
+    timer = micros();
+    ether.clientIcmpRequest(ether.hisip);
   }
 }
